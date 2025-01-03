ce37b24b5435c5d86f797a943c1a8f2ea5b3a412
gitcommit222 <tiomarone02@gmail.com>
2025-01-03 09:31:04 +0800

    add url origin

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL,
+		origin: ["https://www.trioe.dev", `${process.env.FRONTEND_URL}`],
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE"],
 	})
