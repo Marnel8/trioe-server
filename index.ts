1463772203c99ab821ca2d1a2dcf53eb16cd93df
gitcommit222 <tiomarone02@gmail.com>
2025-01-14 15:28:31 +0800

    asd asd

--- index.ts
+++ index.ts
@@ -26,8 +26,8 @@ app.use(cookieParser());
 
 app.use(
 	cors({
-		// origin: "https://www.trioe.dev",
-		origin: "http://localhost:3000",
+		origin: "https://www.trioe.dev",
+		// origin: "http://localhost:3000",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE"],
 	})
